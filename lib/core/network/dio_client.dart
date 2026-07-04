f9bd11a2d39f7f8beaa9654dd9794fc9fc8a10ae
8299515-arch <8299515@gmail.com>
2026-07-04 16:53:45 +0300

    core: add Dio HTTP client

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+
+class DioClient {
+  static final DioClient _instance = DioClient._internal();
+  factory DioClient() => _instance;
+
+  late final Dio dio;
+
+  DioClient._internal() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://api.prorab.plus',
+        connectTimeout: const Duration(seconds: 15),
+        receiveTimeout: const Duration(seconds: 15),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      ),
+    );
+
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          // TODO: attach token
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          return handler.next(response);
+        },
+        onError: (DioException e, handler) {
+          return handler.next(e);
+        },
+      ),
+    );
+  }
+}
