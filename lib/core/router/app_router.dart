447352e3aba381c531938200bf13cfff02533bfc
8299515-arch <8299515@gmail.com>
2026-07-04 16:52:04 +0300

    init: app router (GoRouter baseline)

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../features/dashboard/presentation/dashboard_page.dart';
+
+class AppRouter {
+  static final GoRouter router = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const DashboardPage(),
+      ),
+    ],
+  );
+}
