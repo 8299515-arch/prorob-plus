b8f61917c40c0a0638fd67e2cde6945a27552b1b
8299515-arch <8299515@gmail.com>
2026-08-22 19:14:47 +0300

    feat: add finance domain model

---
+++ lib/features/finance/domain/finance_entry.dart
@@ -0,0 +1,13 @@
+enum FinanceEntryType { income, expense }
+
+class FinanceEntry {
+  const FinanceEntry({required this.id, required this.projectId, required this.type, required this.title, required this.amount, required this.currency, required this.date, this.category});
+  final String id;
+  final String projectId;
+  final FinanceEntryType type;
+  final String title;
+  final double amount;
+  final String currency;
+  final DateTime date;
+  final String? category;
+}
