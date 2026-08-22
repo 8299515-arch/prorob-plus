cf27ce2398c1a22bb4bc6cfc0fccaefff4bcddab
8299515-arch <8299515@gmail.com>
2026-08-22 20:47:00 +0300

    style: format API result

--- lib/core/network/api_result.dart
+++ lib/core/network/api_result.dart
@@ -3,7 +3,10 @@ import 'api_failure.dart';
 sealed class ApiResult<T> {
   const ApiResult();
 
-  R fold<R>({required R Function(T value) success, required R Function(ApiFailure failure) failure}) {
+  R fold<R>({
+    required R Function(T value) success,
+    required R Function(ApiFailure failure) failure,
+  }) {
     return switch (this) {
       ApiSuccess<T>(value: final value) => success(value),
       ApiError<T>(failure: final error) => failure(error),
@@ -13,10 +16,12 @@ sealed class ApiResult<T> {
 
 final class ApiSuccess<T> extends ApiResult<T> {
   const ApiSuccess(this.value);
+
   final T value;
 }
 
 final class ApiError<T> extends ApiResult<T> {
   const ApiError(this.failure);
+
   final ApiFailure failure;
 }
