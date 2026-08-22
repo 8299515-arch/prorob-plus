a6cd3d2ac1262b449bd77656c6ac168b62380bc5
8299515-arch <8299515@gmail.com>
2026-08-22 18:49:13 +0300

    refactor: inject authentication session into application router

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+
+import 'core/auth/auth_session.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
-class ProrabApp extends StatelessWidget {
-  const ProrabApp({super.key});
+class ProrabApp extends StatefulWidget {
+  const ProrabApp({required this.authSession, super.key});
+
+  final AuthSession authSession;
+
+  @override
+  State<ProrabApp> createState() => _ProrabAppState();
+}
+
+class _ProrabAppState extends State<ProrabApp> {
+  late final AppRouter _router;
+
+  @override
+  void initState() {
+    super.initState();
+    _router = AppRouter(widget.authSession);
+  }
+
+  @override
+  void dispose() {
+    _router.dispose();
+    widget.authSession.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Prorab+',
+      title: 'Прораб+',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
-      routerConfig: AppRouter.router,
+      routerConfig: _router.router,
     );
   }
 }
